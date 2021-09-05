582349c508b6d4416de6e4bc82c0833f071596de
Leonardo Herrera <leo2399@gmail.com>
2021-09-04 21:36:34 -0400

    initial commit

---
+++ src/main/java/base/Solution01.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Leonardo Herrera
+ */
+
+package base;
+
+import java.util.Scanner;
+
+public class Solution01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.print("What's your name? ");
+        String name = input.nextLine();
+
+        System.out.println("Hello, " +name+ ", nice to meet you!");
+
+
+    }
+}
